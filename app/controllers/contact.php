2a29271a68790adb4a7e34f3ac0972daab9459a8
Malak Parikh <malakparikh@gmail.com>
2019-12-10 12:00:52 +0530

    Initial Commit

---
+++ app/controllers/contact.php
@@ -0,0 +1,11 @@
+<?php
+class Contact extends Controller
+{
+    public function index(){
+        echo "Contact/index";
+    }
+    public function phone(){
+        echo "Contact/phone";
+    }
+
+}
